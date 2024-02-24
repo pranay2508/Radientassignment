3a3963d79491604ab3ba3c2009a286575ba8a18a
Pranay bhatnagar <bhatnagarpranay8@gmail.com>
2024-02-24 17:04:15 +0530

    updated

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,38 +1,73 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import CheckCircleOutlinedIcon from "@mui/icons-material/CheckCircleOutlined";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+const Container = styled.div``;
 
-const Container = styled.div`
-`
 const Header = styled.div`
-/* text-align: center; */
-padding-left: 275px;
-font-size: 44px;
-padding-top: 22px;
-color: #2C384A;
-font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-`
+  /* text-align: center; */
+  padding-left: 275px;
+  font-size: 50px;
+  padding-top: 22px;
+  color: #2c384a;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+`;
 
 const HorizontalLine = styled.hr`
   border: 0;
-  border-top: 1px solid #E1E4E6; /* Adjust color and thickness as needed */
+  border-top: 1px solid #e1e4e6; /* Adjust color and thickness as needed */
   width: 65%; /* Ensures the line spans the full width of its container */
   margin: 15px 0; /* Adjust margin as needed */
-  margin-left:275px;
+  margin-left: 275px;
+`;
+
+const Headercontainer = styled.div`
+  display: flex;
+`;
+
+const Heading = styled.p`
+  color: #4b5665;
+  padding-top: 4px;
+  font-size: 15px;
+  font-weight: lighter;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+`;
+const Check = styled(CheckCircleOutlinedIcon)`
+  color: #626e79;
+  padding-left: 275px;
+  margin-right: 10px;
+`;
+const Info = styled(InfoOutlinedIcon)`
+  color: #626e79;
+  margin-right: 10px;
 `;
 
 const Main = () => {
   return (
     <Container>
-    <Header>
-    Best website builders in the US
-    </Header>
-    <HorizontalLine/>
-
-
-
+      <Header>Best website builders in the US</Header>
+      <HorizontalLine />
+      <Headercontainer>
+        <Check />
+        <Heading>
+          <div>
+            <p style={{ marginRight: "20px" }}>
+              Last Updated - February 22 , 2020
+            </p>
+          </div>
+        </Heading>
+        <Info />
+        <Heading>
+          <div>
+            <p>Advertising Disclosure </p>
+          </div>
+        </Heading>
+      </Headercontainer>
+      <HorizontalLine />
     </Container>
+  );
+};
 
-  )
-}
-
-export default Main
+export default Main;
